d33d015b29012fd34d204bfd4abc889c06028ed0
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-04-02 15:30:47 +0200

    Fixed first test

--- src/main/java/com/virtuslab/internship/receipt/Receipt.java
+++ src/main/java/com/virtuslab/internship/receipt/Receipt.java
@@ -1,16 +1,18 @@
 package com.virtuslab.internship.receipt;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public record Receipt(
         List<ReceiptEntry> entries,
         List<String> discounts,
-        BigDecimal totalPrice) {
+        BigDecimal totalPrice)
+{
 
     public Receipt(List<ReceiptEntry> entries) {
         this(entries,
-                null,
+                new ArrayList<>(),
                 entries.stream()
                         .map(ReceiptEntry::totalPrice)
                         .reduce(BigDecimal.ZERO, BigDecimal::add)
